20e57e8b30fd6403eeda7e9a62c278bbe9088cca
Santhosh10661 <santhosh10661@gmail.com>
2025-02-24 14:45:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+function App() {
+  const API_KEY =
+    "https://newsapi.org/v2/everything?q=bitcoin&apiKey=2d5475b1c03e42418d72eaa35aca9372";
+  return <></>;
+}
+
+export default App;
